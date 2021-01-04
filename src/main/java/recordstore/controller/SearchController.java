3cd3f2ffe58b05e1d3e8739406bb23c7a641ea5f
Denys_Moiseienko <denis_m_@mail.ru>
2021-01-04 12:50:01 +0100

    change requestMapping in SearchController

--- src/main/java/recordstore/controller/SearchController.java
+++ src/main/java/recordstore/controller/SearchController.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
+@RequestMapping(value = {"/", "/releases", "/labels", "/genres", "/artists"})
 public class SearchController {
 
     private final ArtistService service;
